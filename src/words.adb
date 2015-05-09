8780cac0611a096ab64d63930893bb364175593d
Christopher P. Fair <christopherpfair@comcast.net>
2015-05-08 21:00:05 -0600

    new file:   AUTHORS 	new file:   COPYING 	new file:   ChangeLog 	new file:   INSTALL 	new file:   Makefile.PRECONF 	new file:   Makefile.am 	new file:   NEWS 	new file:   THANKS 	new file:   acinclude.m4 	new file:   ada.m4 	new file:   configure.ac 	new file:   confvars.ads.frame.in 	new file:   install-sh 	new file:   missing 	new file:   src/Makefile.am 	modified:   src/words.adb

--- src/words.adb
+++ src/words.adb
@@ -1,145 +1,113 @@
-   with Ada.Command_Line;
-   with Ada.Text_IO; use Ada.Text_IO;
-   with STRINGS_PACKAGE; use STRINGS_PACKAGE;
-   with CONFIG; use CONFIG;
-   with WORD_PARAMETERS; use WORD_PARAMETERS;
-   with DEVELOPER_PARAMETERS; use DEVELOPER_PARAMETERS;
-   with WORD_PACKAGE; use WORD_PACKAGE;
-   with PARSE;
-   procedure WORDS is
-      INPUT_LINE  : STRING(1..250) := (others => ' ');
-      ARGUMENTS_START : INTEGER := 1;
-   begin
-      --  The language shift in argumants must take place here
-      --  since later parsing of line ignores non-letter characters
-      CONFIGURATION := DEVELOPER_VERSION;
- 
+-- This template is Public Domain.  Have fun.
+--
+-- You really ought to change this opening blurb to reflect your project
+-- information.  One would expect, for example, that it would say something like
+--     (C) Copyright 2015-05-08 by Christopher P. Fair
+-- and give a quick blurb about how to use it and what it does.
 
-      --The main mode of usage for WORDS is a simple call, followed by screen interaction.
-      if Ada.Command_Line.ARGUMENT_COUNT = 0  then      --  Simple WORDS
-         METHOD := INTERACTIVE;                          --  Interactive
-         SUPPRESS_PREFACE := FALSE;
-         SET_OUTPUT(Ada.TEXT_IO.STANDARD_OUTPUT);
-         INITIALIZE_WORD_PARAMETERS;
-         INITIALIZE_DEVELOPER_PARAMETERS;
-         INITIALIZE_WORD_PACKAGE;
-         PARSE;
-         
-      --But there are other, command line options.
-      --WORDS may be called with arguments on the same line, 
-      --in a number of different modes.
-      --
-      else
-         SUPPRESS_PREFACE := TRUE;
-         INITIALIZE_WORD_PARAMETERS;
-         INITIALIZE_DEVELOPER_PARAMETERS;
-         INITIALIZE_WORD_PACKAGE;
-   
-      --Single parameter, either a simple Latin word or an input file.
-      --WORDS amo
-      --WORDS infile
-      if Ada.Command_Line.ARGUMENT_COUNT = 1  then      --  Input 1 word in-line
-         ONE_ARGUMENT:
-         declare
-            INPUT_NAME  : constant STRING := TRIM(Ada.Command_Line.Argument(1));
-         begin
-            OPEN(INPUT, IN_FILE, INPUT_NAME); --  Try file name, not raises NAME_ERROR
-            METHOD := COMMAND_LINE_FILES;
-            SET_INPUT(INPUT);
-            SET_OUTPUT(Ada.TEXT_IO.STANDARD_OUTPUT);
-            PARSE;          --  No additional arguments, so just go to PARSE now
-            exception                  --  Triggers on INPUT
-               when NAME_ERROR  =>                   --  Raised NAME_ERROR therefore
-                  METHOD := COMMAND_LINE_INPUT;      --  Found word in command line
-         end ONE_ARGUMENT;
-      
-      --With two arguments the options are: inputfile and outputfile,
-      --two Latin words, or a language shift to English (Latin being the startup default)
-      --and an English  word (with no part of speech).
-      --WORDS infile outfile
-      --WORDS amo amas
-      --WORDS ^e  love
-      elsif Ada.Command_Line.ARGUMENT_COUNT = 2  then    --  INPUT and OUTPUT files
-         TWO_ARGUMENTS:                                   --  or multiwords in-line
-         declare
-            INPUT_NAME  : constant STRING := TRIM(Ada.Command_Line.Argument(1));
-            OUTPUT_NAME : constant STRING := TRIM(Ada.Command_Line.Argument(2));
-         begin
-           if INPUT_NAME(1) = CHANGE_LANGUAGE_CHARACTER  then
-             if (INPUT_NAME'LENGTH > 1)  then 
-                 CHANGE_LANGUAGE(INPUT_NAME(2));
-                 ARGUMENTS_START := 2;
-                 METHOD := COMMAND_LINE_INPUT;      --  Parse the one word 
-              end if; 
-            else
-               OPEN(INPUT, IN_FILE, INPUT_NAME);
-               CREATE(OUTPUT, OUT_FILE, OUTPUT_NAME);
-               METHOD := COMMAND_LINE_FILES;
-         
-               SET_INPUT(INPUT);
-               SET_OUTPUT(OUTPUT);
-         
-               SUPPRESS_PREFACE := TRUE;
-               OUTPUT_SCREEN_SIZE := INTEGER'LAST;
-               PARSE;           --  No additional arguments, so just go to PARSE now
-         
-               SET_INPUT(Ada.TEXT_IO.STANDARD_INPUT);    --  Clean up
-               SET_OUTPUT(Ada.TEXT_IO.STANDARD_OUTPUT);
-               CLOSE(OUTPUT);
-            end if;
-            exception                  --  Triggers on either INPUT or OUTPUT  !!!
-               when NAME_ERROR  =>
-                  METHOD := COMMAND_LINE_INPUT;            --  Found words in command line
-         
-         end TWO_ARGUMENTS;
-     
-      --With three arguments there could be three Latin words or a language shift
-      --and and English word and part of speech.
-      --WORDS amo amas amat
-      --WORDS ^e love v
-      elsif Ada.Command_Line.ARGUMENT_COUNT = 3  then    --  INPUT and OUTPUT files
-         THREE_ARGUMENTS:                                   --  or multiwords in-line
-         declare
-            ARG1 : constant STRING := TRIM(Ada.Command_Line.Argument(1));
-            ARG2 : constant STRING := TRIM(Ada.Command_Line.Argument(2));
-            ARG3 : constant STRING := TRIM(Ada.Command_Line.Argument(3));
-         begin
-           if ARG1(1) = CHANGE_LANGUAGE_CHARACTER  then
-             if (ARG1'LENGTH > 1)  then 
-                 CHANGE_LANGUAGE(ARG1(2));
-                 ARGUMENTS_START := 2;
-                 METHOD := COMMAND_LINE_INPUT;      --  Parse the one word 
-              end if; 
-            else
-               METHOD := COMMAND_LINE_INPUT;
-            end if;
-             
-         end THREE_ARGUMENTS;
-      
-      --More than three arguments must all be Latin words.
-      --WORDS amo amas amat amamus amatis amant
-      else    --  More than three arguments
-      
-         METHOD := COMMAND_LINE_INPUT;
-      end if;
-   
-   
-      if METHOD = COMMAND_LINE_INPUT  then            --  Process words in command line
-         MORE_ARGUMENTS:
-         begin
-  --Ada.TEXT_IO.PUT_LINE("MORE_ARG  ARG_START = " & INTEGER'IMAGE(ARGUMENTS_START));
-           SUPPRESS_PREFACE := TRUE;
-            for I in ARGUMENTS_START..Ada.Command_Line.Argument_Count  loop  --  Assemble input words 
-               INPUT_LINE := HEAD(TRIM(INPUT_LINE) & " " & Ada.Command_Line.Argument(I), 250);
-            end loop;
-  --Ada.TEXT_IO.PUT_LINE("To PARSE >" & TRIM(INPUT_LINE));
-            PARSE(TRIM(INPUT_LINE));
-         end MORE_ARGUMENTS;
-      end if;
-      end if;
-   
-   end WORDS;
-   
-   
-   
+-- This doesn't actually do anything, except respond to --verbose and --help,
+-- and says hello world.
 
+with Ada.Text_IO;      use Ada.Text_IO;
+with Ada.Command_Line; use Ada.Command_Line;
+
+with confvars; -- Ada version of the environment ./configure set up.
+
+procedure words is
+
+  -- options
+  Verbose:    Boolean := false; -- Are we being verbose?
+  Debug:      Boolean := false; -- Are we debugging?
+  Silent:     Boolean := false; -- Are we being silent?
+  Help:       Boolean := false; -- Do we need help?
+  Version:    Boolean := false; -- Do we need a version?
+
+  -- Anything files after the options?
+  First_File: integer := 0;
+
+  Problems: Boolean := false; -- Did this have junk?
+
+begin
+
+   -- -- Get the options.
+
+   for I in 1 .. Argument_Count loop
+
+      if    Argument (I) = "--verbose"
+         or Argument (I) = "-v" then
+         Verbose := True;
+      elsif Argument (I) = "--silent"
+         or Argument (I) = "--quiet"
+         or Argument (I) = "-s" then
+         Silent := True;
+      elsif Argument (I) = "--debug" then
+         Debug := True;
+      elsif Argument (I) = "--help"
+         or Argument (I) = "-h" then
+         Help  := True;
+      elsif Argument (I) = "--version" then
+         Version := True;
+      elsif Argument (I) = "--" then
+	 if I < Argument_Count then
+            First_File := I + 1;
+	 end if;
+         exit;
+      elsif Argument (I)'Length > 0
+            and then Argument (I) (1) = '-' then
+         Put ("words: Unknown argument "); Put (Argument (I)); New_Line;
+         Problems := True;
+      else -- OK, not an argument... lets assume it is a file.
+         First_File := I;
+         exit;
+      end if;
+
+   end loop;
+   if First_File > Argument_Count then First_File := 0; end if;
+
+   -- Bad arguments are fatal.
+   if Problems then Set_Exit_Status (Failure); return; end if;
+
+
+   -- -- Process argument related options.
+
+   -- Echo out what we have.
+   if Verbose or Debug then
+      Put_Line ("words called...");
+      if Verbose then Put_Line ("  option: --verbose"); end if;
+      if Debug   then Put_Line ("  option: --debug");   end if;
+      if Silent  then Put_Line ("  option: --silent");  end if;
+      if Version then Put_Line ("  option: --version"); end if;
+      if Help    then Put_Line ("  option: --help");    end if;
+      if First_File > 0 then
+         for I in First_file .. Argument_Count loop
+             Put ("    File: "); Put (Argument (I)); New_Line;
+         end loop;
+      end if;
+      Put_Line ("... end of call echo");
+      New_Line;
+   end if;
+
+   if Version then
+      Put_Line ("words Version 0.97");
+   end if;
+
+   if Help then
+      Put_Line ("words [options] [files]");
+      Put_Line ("     --verbose    -v  blather on about everything.");
+      Put_Line ("     --silent     -s  blather about nothing.");
+      Put_Line ("     --help       -h  print this message.");
+      Put_Line ("     --version        Program version.");
+   end if;
+   -- Help and version don't require running the program.
+   if Help or Version then Set_Exit_Status (Success); return; end if;
+
+   ------------------------------------------------------------------
+   -- And actually do something.
+
+   Put_Line ("Guts go here... (Hello there world)");
+   Put      ("This is words, and the GNU tools think the package is called ");
+   Put_Line (confvars.PACKAGE_STRING);
+
+   Set_Exit_Status (Success);
+
+end words;
